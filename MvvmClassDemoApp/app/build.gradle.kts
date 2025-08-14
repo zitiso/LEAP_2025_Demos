117ecdffe13fd2a5e59e371ba2e790beae2afd3f
zitiso <zitiso@gmail.com>
2025-08-14 13:28:15 -0700

    Add test code to repo

--- MvvmClassDemoApp/app/build.gradle.kts
+++ MvvmClassDemoApp/app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
 
 
     testImplementation(libs.junit)
+
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
